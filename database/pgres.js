71ea0746447c3c9d0afb8ba9106c8b6e89d8233d
tartegauri <134502992+tartegauri@users.noreply.github.com>
2023-10-17 20:22:55 +0530

    created tables

--- database/pgres.js
+++ database/pgres.js
@@ -4,11 +4,50 @@ const dotenv = require("dotenv");
 dotenv.config();
 
 const client = new Client(process.env.DB_URL);
+/**
+ * CREATE TABLE CS_USERS_TABLE (
+    order_id SERIAL PRIMARY KEY,
+    username VARCHAR(30) NOT NULL,
+    email VARCHAR(150) NOT NULL,
+    password VARCHAR(20) NOT NULL,
+    picture VARCHAR(255),
+    profession VARCHAR(50)
+);
 
+ */
+/**
+ * CREATE TABLE CS_COURSES_TABLE (
+    course_id SERIAL PRIMARY KEY,
+    course_name VARCHAR(255) NOT NULL,
+    course_price INT NOT NULL CHECK (course_price >= 0),
+    thumbnail VARCHAR(255) NOT NULL,
+    course_description TEXT NOT NULL,
+    course_by VARCHAR(30) NOT NULL
+);
+
+ */
+/**
+ * CREATE TABLE CS_ORDERS_TABLE (
+    order_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES CS_USERS_TABLE(order_id),
+    course_id INT REFERENCES CS_COURSES_TABLE(course_id)
+);
+ * 
+ */
+/**
+ * CREATE TABLE CS_REVIEWS_TABLE (
+    review_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES CS_USERS_TABLE(user_id),
+    course_id INT REFERENCES CS_COURSES_TABLE(course_id),
+    review_description TEXT,
+    review_rating INT
+);
+
+ */
 (async () => {
   await client.connect();
   try {
-    const results = await client.query("SELECT * FROM CS_USER_TABLE");
+    const results = await client.query();
     console.log(results.rows)
     console.log("Database Connection");
   } catch (err) {
